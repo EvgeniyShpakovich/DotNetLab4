824500ed422d0b843ea6d67592d5547efca741c2
EvgeniyShpakovich <vtk241_shyes@student.ztu.edu.ua>
2025-01-30 01:27:09 +0200

    first commit

---
+++ ChatForm/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace ChatForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChatBox = new System.Windows.Forms.TextBox();
+            this.UsernameBox = new System.Windows.Forms.TextBox();
+            this.MessageBox = new System.Windows.Forms.TextBox();
+            this.OnlineUsers = new System.Windows.Forms.ListBox();
+            this.ConnectButton = new System.Windows.Forms.Button();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // ChatBox
+            // 
+            this.ChatBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ChatBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ChatBox.Location = new System.Drawing.Point(12, 53);
+            this.ChatBox.Multiline = true;
+            this.ChatBox.Name = "ChatBox";
+            this.ChatBox.ReadOnly = true;
+            this.ChatBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.ChatBox.Size = new System.Drawing.Size(655, 329);
+            this.ChatBox.TabIndex = 0;
+            // 
+            // UsernameBox
+            // 
+            this.UsernameBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.UsernameBox.Location = new System.Drawing.Point(15, 13);
+            this.UsernameBox.Name = "UsernameBox";
+            this.UsernameBox.Size = new System.Drawing.Size(248, 30);
+            this.UsernameBox.TabIndex = 1;
+            // 
+            // MessageBox
+            // 
+            this.MessageBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MessageBox.Location = new System.Drawing.Point(15, 397);
+            this.MessageBox.Name = "MessageBox";
+            this.MessageBox.Size = new System.Drawing.Size(652, 30);
+            this.MessageBox.TabIndex = 4;
+            // 
+            // OnlineUsers
+            // 
+            this.OnlineUsers.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OnlineUsers.FormattingEnabled = true;
+            this.OnlineUsers.ItemHeight = 25;
+            this.OnlineUsers.Location = new System.Drawing.Point(673, 53);
+            this.OnlineUsers.Name = "OnlineUsers";
+            this.OnlineUsers.Size = new System.Drawing.Size(239, 329);
+            this.OnlineUsers.TabIndex = 4;
+            this.OnlineUsers.SelectedIndexChanged += new System.EventHandler(this.OnlineUsers_SelectedIndexChanged);
+            // 
+            // ConnectButton
+            // 
+            this.ConnectButton.Location = new System.Drawing.Point(269, 13);
+            this.ConnectButton.Name = "ConnectButton";
+            this.ConnectButton.Size = new System.Drawing.Size(107, 30);
+            this.ConnectButton.TabIndex = 5;
+            this.ConnectButton.Text = "Connect";
+            this.ConnectButton.UseVisualStyleBackColor = true;
+            this.ConnectButton.Click += new System.EventHandler(this.ConnectButton_Click);
+            // 
+            // SendButton
+            // 
+            this.SendButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.SendButton.Location = new System.Drawing.Point(673, 391);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(239, 36);
+            this.SendButton.TabIndex = 6;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(720, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(153, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Online Users";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(920, 439);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.ConnectButton);
+            this.Controls.Add(this.OnlineUsers);
+            this.Controls.Add(this.MessageBox);
+            this.Controls.Add(this.UsernameBox);
+            this.Controls.Add(this.ChatBox);
+            this.MaximumSize = new System.Drawing.Size(938, 486);
+            this.MinimumSize = new System.Drawing.Size(938, 486);
+            this.Name = "Form1";
+            this.Text = "Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox ChatBox;
+        private System.Windows.Forms.TextBox UsernameBox;
+        private System.Windows.Forms.TextBox MessageBox;
+        private System.Windows.Forms.ListBox OnlineUsers;
+        private System.Windows.Forms.Button ConnectButton;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ChatForm/Form1.cs
@@ -0,0 +1,132 @@
+﻿using ChatLibrary;
+using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ChatForm
+{
+    [CallbackBehavior(ConcurrencyMode = ConcurrencyMode.Reentrant)]
+    public partial class Form1 : Form, IChatCallback
+    {
+        private IChatService _chatService;
+        private DuplexChannelFactory<IChatService> _factory;
+        private string _username;
+        private string _selectedUser;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task ConnectToServerAsync()
+        {
+            try
+            {
+                _factory = new DuplexChannelFactory<IChatService>(this, new NetTcpBinding(),
+                    new EndpointAddress("net.tcp://localhost:8000/ChatService"));
+                _chatService = _factory.CreateChannel();
+
+                var connected = await _chatService.ConnectAsync(_username);
+
+                if (connected)
+                {
+                    ChatBox.AppendText("Connected to chat." + Environment.NewLine);
+                    ConnectButton.Enabled = false;
+                    UsernameBox.Enabled = false;
+                }
+                else
+                {
+                    ChatBox.AppendText("Failed to connect." + Environment.NewLine);
+                }
+            }
+            catch
+            {
+                ChatBox.AppendText("Failed to connect." + Environment.NewLine);
+            }
+        }
+
+        public void ReceiveMessage(string message)
+        {
+            ChatBox.Invoke(new MethodInvoker(() => ChatBox.Text += message + Environment.NewLine));
+        }
+
+        public void UpdateUserList(List<string> users)
+        {
+            if (OnlineUsers.InvokeRequired)
+            {
+                OnlineUsers.Invoke(new Action(() =>
+                {
+                    OnlineUsers.Items.Clear();
+                    foreach (var user in users)
+                    {
+                        OnlineUsers.Items.Add(user);
+                    }
+                }));
+            }
+            else
+            {
+                OnlineUsers.Items.Clear();
+                foreach (var user in users)
+                {
+                    OnlineUsers.Items.Add(user);
+                }
+            }
+        }
+        private async void ConnectButton_Click(object sender, EventArgs e)
+        {
+            _username = UsernameBox.Text;
+            await ConnectToServerAsync();
+        }
+
+        private async void SendButton_Click(object sender, EventArgs e)
+        {
+            var message = MessageBox.Text;
+            var currentTime = DateTime.Now.ToString("HH:mm");
+
+            if (!string.IsNullOrEmpty(_selectedUser) && _selectedUser != _username)
+            {
+                ChatBox.AppendText($"[{currentTime}] ");
+               await _chatService.SendPrivateMessageAsync(message,  _selectedUser, _username);
+                ChatBox.AppendText($"[You -> {_selectedUser}]: {message}" + Environment.NewLine);
+            }
+            else
+            {
+                await _chatService.SendMessageToGroupAsync(message, _username);
+            }
+            ChatBox.SelectionStart = ChatBox.Text.Length;
+            ChatBox.ScrollToCaret();
+
+            MessageBox.Clear();
+        }
+
+        private async void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (_chatService != null && !string.IsNullOrEmpty(_username))
+            {
+                await _chatService.DisconnectAsync(_username);
+            }
+        }
+
+        private void OnlineUsers_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (OnlineUsers.SelectedItem != null)
+            {
+                if (_selectedUser != null && _selectedUser == OnlineUsers.SelectedItem.ToString())
+                {
+                    OnlineUsers.ClearSelected();
+                    _selectedUser = null; 
+                }
+                else
+                {
+                    _selectedUser = OnlineUsers.SelectedItem.ToString();
+                }
+            }
+            else
+            {
+                _selectedUser = null;
+            }
+        }
+    }
+}
